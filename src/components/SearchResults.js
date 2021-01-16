d0faf5613066d0f0924d8cba58b5cebf0ba3c75b
Duru-young Raymond <duruyoungcr@gmail.com>
2021-01-16 15:16:13 +0100

    Update: Minor UI changes

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,7 +7,7 @@ const SearchResults = ({results, loading, setNomMovie, movies, nomIDS}) => {
     return (
         <div className="results-container">
             <p className="title">Search Results</p>
-            {results.Response === "True" && <p>We found {results.totalResults} matches for your search</p>}
+            {results.Response === "True" && <p className="sub-title">We found {results.totalResults} matches for your search, Here are the top 10 matches</p>}
             {loading && <Loader/>}
             <DisplayMovies movies={movies} setNomMovie={setNomMovie} nomIDS={nomIDS}/>
         </div>
